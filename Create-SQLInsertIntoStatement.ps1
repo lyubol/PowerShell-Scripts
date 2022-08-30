a0b2a65d7fed0ee5d42a2c0193c5fd1c6d674058
Lyubomir Lirkov <L_L@adatis.co.uk>
2022-08-30 22:22:25 +0300

    Updates to function Create-ValuesStatement

--- Create-SQLInsertIntoStatement.ps1
+++ Create-SQLInsertIntoStatement.ps1
@@ -23,22 +23,22 @@ function Create-InsertStatement{
 
 # Function to return the "VALUES" part of the insert statement
 function Create-ValuesStatement{
-    Write-Output "VALUES($($CSVFile[0].Id), '$($CSVFile[0].FirstName)', '$($CSVFile[0].LastName)', $($CSVFile[0].Salary)),"
+    Write-Output "VALUES('$($CSVFile[0].Id)', '$($CSVFile[0].FirstName)', '$($CSVFile[0].LastName)', '$($CSVFile[0].Salary)'),"
     foreach($i in 1..$($CSVFile[0].psobject.properties.name.Length))
     {
         foreach($LINE in $CSVFile[$i])
         {
             if ($i -eq $CSVFile[0].psobject.properties.name.Length) {
-                "    ($($LINE.Id), '$($LINE.FirstName)', '$($LINE.LastName)', $($LINE.Salary))"
+                "    ('$($LINE.Id)', '$($LINE.FirstName)', '$($LINE.LastName)', '$($LINE.Salary)')"
             }else {
-                "    ($($LINE.Id), '$($LINE.FirstName)', '$($LINE.LastName)', $($LINE.Salary)),"
+                "    ('$($LINE.Id)', '$($LINE.FirstName)', '$($LINE.LastName)', '$($LINE.Salary)'),"
             }  
         }
     }
 
 }
 
-# Read source CSV file
+# Read CSV file
 $CSVFile = Import-Csv "C:\Users\L_L\Desktop\Employees.csv"
 
 # Execute functions to get the full "INSERT" statement
